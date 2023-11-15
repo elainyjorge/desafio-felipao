b7638a78baf3a1ed92b7c0ac0ce9992bed3ee0e3
Elainy Jorge Silva <112271271+elainyjorge@users.noreply.github.com>
2023-11-15 18:28:57 -0300

    inclusao laço de repetição

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
-// Desafio Classificar de nível de Herói
+// Desafio Classificador de nível de Herói
 
-const nomeHeroi = prompt("Digite o nome do herói: ")
-const quantidadeXP = Number(prompt("Digite a quantidade de experiência do héroi(De 0 a 10000)"))
-let nivel
+const quantidadeHerois = Number(prompt("Digite o número de heróis: "))
 
-if(quantidadeXP <= 1000){
-  nivel = "Ferro"
-} else if(quantidadeXP >= 1001 && quantidadeXP <= 2000){
-  nivel = "Bronze"
-} else if (quantidadeXP >= 2001 && quantidadeXP <= 6000){
-  nivel = "Prata"
-} else if(quantidadeXP >= 6001 && quantidadeXP <= 7000){
-  nivel = "Ouro"
-} else if(quantidadeXP >= 7001 && quantidadeXP <= 8000){
-  nivel = "Platina"
-} else if(quantidadeXP >= 8001 && quantidadeXP <= 9000){
-  nivel = "Ascendente"
-} else if(quantidadeXP >= 9001 && quantidadeXP <= 10000){
-  nivel = "Imortal"
-} else {
-  nivel = "Radiante"
-} 
+for(let i=0; i <= quantidadeHerois; i++){
+  const nomeHeroi = prompt("Digite o nome do herói: ")
+  const quantidadeXP = Number(prompt("Digite a quantidade de experiência do héroi(De 0 a 10000)"))
 
-console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivel}`)
+  let nivel
+  
+  if(quantidadeXP <= 1000){
+    nivel = "Ferro"
+  } else if(quantidadeXP >= 1001 && quantidadeXP <= 2000){
+    nivel = "Bronze"
+  } else if (quantidadeXP >= 2001 && quantidadeXP <= 6000){
+    nivel = "Prata"
+  } else if(quantidadeXP >= 6001 && quantidadeXP <= 7000){
+    nivel = "Ouro"
+  } else if(quantidadeXP >= 7001 && quantidadeXP <= 8000){
+    nivel = "Platina"
+  } else if(quantidadeXP >= 8001 && quantidadeXP <= 9000){
+    nivel = "Ascendente"
+  } else if(quantidadeXP >= 9001 && quantidadeXP <= 10000){
+    nivel = "Imortal"
+  } else {
+    nivel = "Radiante"
+  } 
+  console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivel}`)
+}
